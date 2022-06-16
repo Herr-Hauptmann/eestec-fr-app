7add55e6bfb6a5a9043ee3dc66feaa0a09aafbef
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-06-16 18:30:25 +0200

    fix allignments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,16 +20,16 @@
                 @foreach ($statuses as $status)
                 <li class="list-group-item">
                     <div class="row">
-                        <div class="col-6 col-md-3 d-flex justify-content-center">
+                        <div class="col-6 col-md-3 d-flex justify-content-center my-auto">
                             {{$status->company->name}}
                         </div>
-                        <div class="col-6 col-md-3 d-flex justify-content-center">
+                        <div class="col-6 col-md-3 d-flex justify-content-center my-auto">
                             {{$status->event->name}}
                         </div>
-                        <div class="col-6 col-md-3 d-flex justify-content-center">
+                        <div class="col-6 col-md-3 d-flex justify-content-center my-auto">
                             {{$status->statusText()}}
                         </div>
-                        <div class="col-6 col-md-3 d-flex justify-content-center">
+                        <div class="col-6 col-md-3 d-flex justify-content-center my-auto">
 
                             <a href="/companies/{{$status->company->id}}" class="btn btn-primary mx-3">View Company</a>
                         </div>
