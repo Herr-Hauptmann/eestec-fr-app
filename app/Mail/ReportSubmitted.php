c231d231fba4bd5f4e5657e81096703d12a79888
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-07-07 16:45:45 +0200

    fix bug, mails now sent to coordinators, mails now look good

--- app/Mail/ReportSubmitted.php
+++ app/Mail/ReportSubmitted.php
@@ -16,23 +16,22 @@ class ReportSubmitted extends Mailable
     public $content;
     public $event;
     public $company;
+    public $userName;
+    public $coordinator;
 
-    public function __construct(String $content, Event $event, Company $company)
+    public function __construct(String $content, Event $event, Company $company, String $userName, String $coordinator)
     {
         $this->content=$content;
         $this->event=$event;
         $this->company=$company;
+        $this->userName = $userName;
+        $this->coordinator = $coordinator;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
         return $this->from('fr-app@eestec-sa.ba')
-                    ->subject('['.$this->event->name.']['.$this->company->name.'] New report')
-                    ->view('report-email');
+                    ->subject('[EESTEC]['.$this->event->name.']['.$this->company->name.'] New report')
+                    ->view('emails.report-email');
     }
 }
