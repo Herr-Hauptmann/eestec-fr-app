822573262643c39830846a47f43102d72a1e204a
ezolota1 <ezolota1@etf.unsa.ba>
2022-07-15 12:59:53 +0200

    implemented search option for contacting dropdown (company and contacting member)

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -179,4 +179,16 @@
         </ol>
     </div>
     <script src="{{ asset('js/events.js')}}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(function(){
+            $("#company_id").select2();
+        }); 
+    </script>
+    <script>
+        $(function(){
+            $("#user_id").select2();
+        }); 
+    </script>
 </x-app-layout>
